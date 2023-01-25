4db2f53cc55302c0976c6b090689d3696f998fc2
Alex Chow <zhouchuanglin@gmail.com>
2023-01-25 16:40:09 +0100

    动态模块载入加入版本标识,避免缓存造成功能不匹配

--- src/BootstrapBlazor.Bluetooth/BatteryLevel.razor.cs
+++ src/BootstrapBlazor.Bluetooth/BatteryLevel.razor.cs
@@ -63,7 +63,7 @@ public partial class BatteryLevel : IAsyncDisposable
             if (firstRender)
             {
                 Device??= new BluetoothDevice();
-                module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Bluetooth/lib/batterylevel/app.js");
+                module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Bluetooth/lib/batterylevel/app.js" + "?v=" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
                 InstanceBatteryLevel = DotNetObjectReference.Create(this);
                 //await module.InvokeVoidAsync("getBatteryLevel", InstanceBatteryLevel, BatteryLevelElement);
             }
--- src/BootstrapBlazor.Bluetooth/Heartrate.razor.cs
+++ src/BootstrapBlazor.Bluetooth/Heartrate.razor.cs
@@ -63,7 +63,7 @@ public partial class Heartrate : IAsyncDisposable
             if (firstRender)
             {
                 Device??= new BluetoothDevice();
-                module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Bluetooth/lib/heartrate/app.js");
+                module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Bluetooth/lib/heartrate/app.js" + "?v=" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
                 InstanceHeartrate = DotNetObjectReference.Create(this);
             }
         }

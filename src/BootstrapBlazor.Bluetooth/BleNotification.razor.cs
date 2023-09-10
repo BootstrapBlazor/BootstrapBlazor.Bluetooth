0afbd9f13cf849d5fd64835c35efbe0150c1df77
Alex chow <zhouchuanglin@gmail.com>
2023-09-10 22:58:43 +0200

    BleNotification

---
+++ src/BootstrapBlazor.Bluetooth/BleNotification.razor.cs
@@ -0,0 +1,174 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.ComponentModel;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 蓝牙通知 BleNotification 组件
+/// </summary>
+public partial class BleNotification : IAsyncDisposable
+{
+    [Inject] IJSRuntime? JS { get; set; }
+    private IJSObjectReference? module;
+    private DotNetObjectReference<BleNotification>? InstanceNotification { get; set; }
+
+    /// <summary>
+    /// UI界面元素的引用对象
+    /// </summary>
+    public ElementReference NotificationElement { get; set; }
+
+    /// <summary>
+    /// 获得/设置 蓝牙设备
+    /// </summary>
+    [Parameter]
+    public BluetoothDevice? Device { get; set; } = new BluetoothDevice();
+
+    /// <summary>
+    /// 获得/设置 状态更新回调方法
+    /// </summary>
+    [Parameter]
+    public Func<BluetoothDevice, Task>? OnUpdateStatus { get; set; }
+
+    /// <summary>
+    /// 获得/设置 数值更新回调方法
+    /// </summary>
+    [Parameter]
+    public Func<int, Task>? OnUpdateValue { get; set; }
+
+    /// <summary>
+    /// 获得/设置 错误更新回调方法
+    /// </summary>
+    [Parameter]
+    public Func<string, Task>? OnUpdateError { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示log
+    /// </summary>
+    [Parameter]
+    public bool Debug { get; set; }
+
+    /// <summary>
+    /// 服务UUID / Service UUID
+    /// </summary>
+    [Parameter]
+    [DisplayName("服务UUID / Service UUID")]
+    public object? ServiceUuid { get; set; } = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+
+    /// <summary>
+    /// 特征UUID / Characteristic UUID
+    /// </summary>
+    /// <returns></returns>
+    [DisplayName("特征UUID / Characteristic UUID")]
+    public object? CharacteristicUuid { get; set; } = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        try
+        {
+            if (firstRender)
+            {
+                Device??= new BluetoothDevice();
+                module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Bluetooth/BleNotification.razor.js" + "?v=" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+                InstanceNotification = DotNetObjectReference.Create(this);
+            }
+        }
+        catch (Exception e)
+        {
+            if (OnUpdateError != null) await OnUpdateError.Invoke(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// 获取蓝牙低功耗设备BLE的特征通知
+    /// </summary>
+    public virtual async Task GetNotification()
+    {
+        try
+        {
+            await module!.InvokeVoidAsync("Notification", InstanceNotification, NotificationElement, "getNotification", ServiceUuid, CharacteristicUuid);
+        }
+        catch (Exception e)
+        {
+            if (OnUpdateError != null) await OnUpdateError.Invoke(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// 停止监听BLE的特征通知
+    /// </summary>
+    public virtual async Task StopNotification()
+    {
+        try
+        {
+            await module!.InvokeVoidAsync("Notification", InstanceNotification, NotificationElement, "stopNotification");
+        }
+        catch (Exception e)
+        {
+            if (OnUpdateError != null) await OnUpdateError.Invoke(e.Message);
+        }
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        InstanceNotification?.Dispose();
+        if (module is not null)
+        {
+            await module.DisposeAsync();
+        }
+    }
+
+    /// <summary>
+    /// 设备名称回调方法
+    /// </summary>
+    /// <param name="devicename"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task UpdateDevicename(string devicename)
+    {
+        Device!.Name = devicename;
+        if (OnUpdateStatus != null) await OnUpdateStatus.Invoke(Device);
+    }
+
+    /// <summary>
+    /// 设备特征通知回调方法
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task UpdateValue(int value)
+    {
+        Device!.Value = value;
+        if (OnUpdateValue != null) await OnUpdateValue.Invoke(value);
+    }
+
+    /// <summary>
+    /// 状态更新回调方法
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task UpdateStatus(string status)
+    {
+        Device!.Status = status;
+        if (OnUpdateStatus != null) await OnUpdateStatus.Invoke(Device);
+    }
+
+    /// <summary>
+    /// 错误更新回调方法
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task UpdateError(string status)
+    {
+        if (OnUpdateError != null) await OnUpdateError.Invoke(status);
+    }
+
+}
---
+++ src/BootstrapBlazor.Bluetooth/BleNotification.razor.js
@@ -0,0 +1,89 @@
+﻿let myCharacteristic;
+
+export function notification(wrapper, element, callfunction =null, serviceUuid = 'heart_rate', characteristicUuid = 'heart_rate_measurement') {
+    if (!element.id) element = document;
+    const log = element.querySelector("[data-action=log]");
+    const notificationValue = document.querySelector("[data-action=notificationValue]");
+
+    if (callfunction && callfunction == "stopNotification") {
+        console.log('stopNotification');
+        stopNotification();
+        return;
+    }
+
+    getNotification();
+
+    async function getNotification() {
+        if (notificationValue) notificationValue.innerHTML = '--';
+        if (characteristicUuid.startsWith('0x')) {
+            characteristicUuid = parseInt(characteristicUuid);
+        }
+
+        try {
+            logII('Requesting Bluetooth Device...', serviceUuid);
+            const device = await navigator.bluetooth.requestDevice({
+                filters: [{ services: [serviceUuid] }]
+            });
+            wrapper.invokeMethodAsync('UpdateDevicename', device.devicename);
+
+            logII('Connecting to GATT Server...');
+            const server = await device.gatt.connect();
+
+            wrapper.invokeMethodAsync('UpdateStatus', 'Getting Service...');
+            logII('Getting Service...');
+            const service = await server.getPrimaryService(serviceUuid);
+
+            logII('Getting Characteristic...');
+            myCharacteristic = await service.getCharacteristic(characteristicUuid);
+
+            await myCharacteristic.startNotifications();
+
+            logII('> Notifications started');
+            myCharacteristic.addEventListener('characteristicvaluechanged',
+                handleNotifications);
+        } catch (error) {
+            logErr('Argh! ' + error);
+        }
+    }
+
+    async function stopNotification() {
+        if (myCharacteristic) {
+            try {
+                await myCharacteristic.stopNotifications();
+                logII('> Notifications stopped');
+                myCharacteristic.removeEventListener('characteristicvaluechanged',
+                    handleNotifications);
+            } catch (error) {
+                logErr('Argh! ' + error);
+            }
+        }
+    }
+
+    function handleNotifications(event) {
+        let value = event.target.value;
+        let a = [];
+        // Convert raw data bytes to hex values just for the sake of showing something.
+        // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+        // TextDecoder to process raw data bytes.
+        for (let i = 0; i < value.byteLength; i++) {
+            a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+        }
+        let result = a.join(' ');
+        wrapper.invokeMethodAsync('UpdateValue', result);
+        logII('> ' + result);
+        if (result) notificationValue.innerHTML = result;
+    }
+
+    function logII(info) {
+        if (log) log.textContent += info + '\n';
+        console.log(info);
+        wrapper.invokeMethodAsync('UpdateStatus', info);
+    }
+
+    function logErr(info) {
+        if (log) log.textContent += info + '\n';
+        console.log(info);
+        wrapper.invokeMethodAsync('UpdateError', info);
+    }
+
+}
